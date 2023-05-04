fixed rotation order

rc2nerf.py
@@ -144,9 +144,9 @@ if __name__ == "__main__":
 
             mat = np.eye(4)
 
-            mat[:3, :3] = matrix_from_euler([row['pitch'], row['heading'], row['roll']], 'yxz', True)
+            mat[:3, :3] = matrix_from_euler([row['roll'], row['pitch'], row['heading']], 'yxz', True)
 
-            mat[:3,3] = [row['x'], row['y'], row['alt']] * args.scale
+            mat[:3,3] = np.array([row['x'], row['y'], row['alt']]) * float(args.scale)
 
             camera['transform_matrix'] = Mat2Nerf(mat)
 